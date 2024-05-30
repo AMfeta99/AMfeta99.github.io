381b19d2e1eae233f73e4c52acd26f7123c53753
Ana Maria Sousa <74252797+AMfeta99@users.noreply.github.com>
2024-05-30 23:24:57 +0100

    size and ajusting text

--- assets/js/pub.js
+++ assets/js/pub.js
@@ -1,10 +1,13 @@
 document.querySelectorAll('.clickable').forEach(item => {
     item.addEventListener('click', event => {
         const remainingContent = event.target.nextElementSibling;
+        const clickIndicator = event.target.querySelector('.click-indicator');
         if (remainingContent.style.display === 'none' || remainingContent.style.display === '') {
             remainingContent.style.display = 'block';
+            clickIndicator.style.display = 'none';
         } else {
             remainingContent.style.display = 'none';
+            clickIndicator.style.display = 'inline';
         }
     });
 });
