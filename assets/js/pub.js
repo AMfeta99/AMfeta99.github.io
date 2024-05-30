fea1489c0980410be041c8d41990fea4e2841405
Ana Maria Sousa <74252797+AMfeta99@users.noreply.github.com>
2024-05-30 23:45:17 +0100

    click update

--- assets/js/pub.js
+++ assets/js/pub.js
@@ -1,13 +1,12 @@
 document.querySelectorAll('.clickable').forEach(item => {
     item.addEventListener('click', event => {
-        const remainingContent = event.target.nextElementSibling;
-        const clickIndicator = event.target.querySelector('.click-indicator');
+        const remainingContent = event.target.closest('.description').querySelector('.remaining-content');
         if (remainingContent.style.display === 'none' || remainingContent.style.display === '') {
             remainingContent.style.display = 'block';
-            clickIndicator.style.display = 'none';
+            event.target.style.display = 'none';
         } else {
             remainingContent.style.display = 'none';
-            clickIndicator.style.display = 'inline';
+            event.target.style.display = 'inline';
         }
     });
 });
