32d778c84e51f42547657074e05454a786c31907
Ana Maria Sousa <74252797+AMfeta99@users.noreply.github.com>
2025-01-23 15:43:42 +0000

    js

--- assets/js/certificados.js
+++ assets/js/certificados.js
@@ -1,4 +1,12 @@
-function toggleDescription(element) {
+function toggleContent(element) {
+    const imageContainer = element.previousElementSibling;
     const descricao = element.nextElementSibling;
+
+    // Toggle visibility of the image
+    if (imageContainer) {
+        imageContainer.style.display = imageContainer.style.display === 'none' ? 'block' : 'none';
+    }
+
+    // Toggle visibility of the description
     descricao.classList.toggle('hidden');
 }
