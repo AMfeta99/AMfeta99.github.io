ecae8d0c690d003e84c3568910ac6e6acdca985e
Ana Maria Sousa <74252797+AMfeta99@users.noreply.github.com>
2025-01-23 16:14:30 +0000

    Update certificados.js

--- assets/js/certificados.js
+++ assets/js/certificados.js
@@ -5,12 +5,32 @@
     descricao.classList.toggle('hidden');
 } */
 
-    function toggleContent(element) {
+/*     function toggleContent(element) {
         const parentCard = element.parentElement;
         const imageContainer = parentCard.querySelector('.image-container');
         const descricao = parentCard.querySelector('.descricao');
     
-        // Toggle visibility of the image
-        if (imageContainer) {
+            // Toggle visibility of the image
+            if (imageContainer) {
+                imageContainer.style.display = imageContainer.style.display === 'none' ? 'block' : 'none';
+            }
+        
+            // Toggle visibility of the description
+            descricao.classList.toggle('hidden');
+        }
+     */
     
-    
+
+
+function toggleContent(element) {
+    const imageContainer = element.previousElementSibling;
+    const descricao = element.nextElementSibling;
+
+    // Toggle visibility of the image
+    if (imageContainer) {
+        imageContainer.style.display = imageContainer.style.display === 'none' ? 'block' : 'none';
+    }
+
+    // Toggle visibility of the description
+    descricao.classList.toggle('hidden');
+}
