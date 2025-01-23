097825b390d83d23042c0793b0e9d0f053eb9f19
Ana Maria Sousa <74252797+AMfeta99@users.noreply.github.com>
2025-01-23 16:12:28 +0000

    js

--- assets/js/certificados.js
+++ assets/js/certificados.js
@@ -1,6 +1,16 @@
-function toggleContent(element) {
+/* function toggleContent(element) {
     const descricao = element.nextElementSibling;
 
     // Toggle visibility of the description for the clicked section
     descricao.classList.toggle('hidden');
-}
+} */
+
+    function toggleContent(element) {
+        const parentCard = element.parentElement;
+        const imageContainer = parentCard.querySelector('.image-container');
+        const descricao = parentCard.querySelector('.descricao');
+    
+        // Toggle visibility of the image
+        if (imageContainer) {
+    
+    
