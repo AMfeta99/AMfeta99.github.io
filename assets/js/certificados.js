cc323e0320f8111d3b362bd1bc1b58144b73d49f
Ana Maria Sousa <74252797+AMfeta99@users.noreply.github.com>
2025-01-23 15:53:23 +0000

    js

--- assets/js/certificados.js
+++ assets/js/certificados.js
@@ -1,13 +1,6 @@
 function toggleContent(element) {
-    const parentCard = element.parentElement;
-    const imageContainer = parentCard.querySelector('.image-container');
-    const descricao = parentCard.querySelector('.descricao');
+    const descricao = element.nextElementSibling;
 
-    // Toggle visibility of the image
-    if (imageContainer) {
-        imageContainer.style.display = imageContainer.style.display === 'none' ? 'block' : 'none';
-    }
-
-    // Toggle visibility of the description
+    // Toggle visibility of the description for the clicked section
     descricao.classList.toggle('hidden');
 }
